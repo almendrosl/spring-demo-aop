e097e69ccf01d831624b68740c3336ff831e4523
Lucas Almendros <almendroslm@gmail.com>
2021-02-15 19:34:35 -0300

    pintcut declaration

--- src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,20 +2,28 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
 
-//    @Before("execution(public void add*())")
-//    @Before("execution(* add*(com.luv2code.aopdemo.Account))")
-//    @Before("execution(* add*(com.luv2code.aopdemo.Account, ..))")
-//    @Before("execution(* add*(..))")
-    @Before("execution(* com.luv2code.aopdemo.dao.*.*(..))") //Match all methods in the package
+    @Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+    private void forDaoPackage() {}
+
+    @Before("forDaoPackage()")
     public void beforeAddAccountAdvice() {
 
         System.out.println("\n=======>>>>>>>>> Executing @Before advice on addAccount()");
+
+    }
+
+
+    @Before("forDaoPackage()") //Match all methods in the package
+    public void performApiAnalytics() {
+
+        System.out.println("\n=======>>>>>>>>> Performing API analytics");
 
     }
 
