c6392ac548468f14280db9467dccd48305f1bc5a
Lucas Almendros <almendroslm@gmail.com>
2021-02-15 21:11:35 -0300

    after returning

---
+++ src/com/luv2code/aopdemo/AfterReturningDemoApp.java
@@ -0,0 +1,28 @@
+package com.luv2code.aopdemo;
+
+import com.luv2code.aopdemo.dao.AccountDAO;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+public class AfterReturningDemoApp {
+
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(DemoConfig.class);
+
+        AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+
+        List<Account> theAccounts = theAccountDAO.findAccounts();
+
+        System.out.println("\n\nMain Program: AfterReturningDemoApp");
+        System.out.println("-----");
+
+        System.out.println(theAccounts);
+
+        System.out.println("\n");
+
+        context.close();
+    }
+}
--- src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,16 +2,47 @@ package com.luv2code.aopdemo.aspect;
 
 import com.luv2code.aopdemo.Account;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Locale;
+
 @Aspect
 @Component
 @Order(2)
 public class MyDemoLoggingAspect {
+
+    @AfterReturning(
+            pointcut = "execution(* com.luv2code.aopdemo.dao.AccountDAO.findAccounts(..))",
+            returning = "result"
+    )
+    public void afterReturningFindAccountsAdvice(
+            JoinPoint theJoinPoint, List<Account> result) {
+
+        String method = theJoinPoint.getSignature().toShortString();
+
+        System.out.println("\n======>>> Executing @AfterReturning on method: " + method);
+
+        System.out.println("\n======>>> result is: " + result);
+
+        convertAccountNamesToUpperCase(result);
+
+        System.out.println("\n======>>> result is: " + result);
+    }
+
+    //Modifica el resultado antes de retornarlo
+    private void convertAccountNamesToUpperCase(List<Account> result) {
+        for (Account account : result) {
+            String theUpperName = account.getName().toUpperCase(Locale.ROOT);
+            account.setName(theUpperName);
+        }
+    }
+
 
     @Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
     public void beforeAddAccountAdvices(JoinPoint theJoinPoint) {
